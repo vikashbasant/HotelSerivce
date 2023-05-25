5e98ec6c4e536ce6dc86c03cb09d2b819913a20f
Vikash Basant <basantvikash360@gmail.com>
2023-05-25 15:22:30 +0530

    Implementing Service Discovery client in Hotel microservice

--- src/main/java/com/lm/hotel/HotelSerivceApplication.java
+++ src/main/java/com/lm/hotel/HotelSerivceApplication.java
@@ -2,8 +2,10 @@ package com.lm.hotel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HotelSerivceApplication {
 
 	public static void main(String[] args) {
