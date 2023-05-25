7fa730a3c1906fc33f5bd05db1c765175dfa0693
Vikash Basant <basantvikash360@gmail.com>
2023-05-25 15:49:11 +0530

    correct the some spelling mistake of main method of the project

--- src/main/java/com/lm/hotel/HotelServiceApplication.java
+++ src/main/java/com/lm/hotel/HotelServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class HotelSerivceApplication {
+public class HotelServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HotelSerivceApplication.class, args);
+		SpringApplication.run(HotelServiceApplication.class, args);
 	}
 
 }
